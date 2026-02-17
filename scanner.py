05f4a0fae5b87cfa516e6af014fcbb82d78d5d56
pixelee <pixeleitt@naver.com>
2026-02-17 20:47:03 +0900

    배너 프린트 시도(실패)

--- scanner.py
+++ scanner.py
@@ -8,19 +8,23 @@ target_ip = input("스캔할 IP를 입력하세요 (예: 127.0.0.1): ")
 print(f"{Fore.CYAN}--- [{target_ip}] 스캔 시작 ---{Fore.RESET}")
 
 # 자주 쓰는 포트 몇 개만 스캔 (원래는 1~65535)
-target_ports = [21, 22, 80, 443, 3306, 8080]
+target_ports = [21, 22, 80, 135, 443, 445, 3306, 8080,11434]
 
 for port in target_ports:
     try:
         # 소켓 생성 (IPv4, TCP)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1) # 1초 안에 응답 없으면 패스
-        
         # 접속 시도
         result = sock.connect_ex((target_ip, port))
         
         if result == 0:
             print(f"{Fore.GREEN}[+] Port {port}: OPEN{Fore.RESET}")
+            try:
+                banner = sock.recv(1024).decode().strip()
+                print(f"    Banner: {banner}")
+            except:
+                print("    Banner: (없음)")
         else:
             print(f"{Fore.RED}[-] Port {port}: CLOSED{Fore.RESET}")
             
